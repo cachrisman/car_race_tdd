27ed0c87bb5d94fad713c946384d70b60502babf
cachrisman <charlie.chrisman@gmail.com>
2015-06-03 15:50:58 -0700

    solutions

--- lib/race.rb
+++ lib/race.rb
@@ -1,5 +1,16 @@
 require_relative 'car'
 
 class Race
-  #write Race class code here
+  attr_reader :cars
+  def initialize
+    @cars = [Car.new, Car.new]
+    cars.first.accelerate rand(100)
+    cars.last.accelerate rand(100)
+  end
+  def winner
+    cars.first.speed > cars.last.speed ? cars.first : cars.last
+  end
+  def loser
+    cars.first.speed > cars.last.speed ? cars.last : cars.first
+  end
 end
